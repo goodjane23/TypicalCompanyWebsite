d722d6a18c61036671e74ac88a6c3055e3b5b557
goodjane23 <apochemubinet@gmail.com>
2023-07-25 19:29:56 +0300

    Add project files.

---
+++ TypicalCompanyWebsite/ClientApp/src/app/data-row/data-row.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'data-row',
+  templateUrl: './data-row.component.html',
+  styleUrls: ['./data-row.component.css']
+})
+export class DataTemplate { }
---
+++ TypicalCompanyWebsite/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  title = 'О компании';
+  contains = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquam erat at erat lacinia, sit amet tempor leo rhoncus. Sed eget urna sit amet odio suscipit efficitur. Duis at sem mattis, venenatis lectus a, rhoncus justo. Proin in mauris egestas, malesuada orci a, lacinia arcu. Donec sagittis semper metus, et maximus neque convallis eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec venenatis dolor vel rutrum vehicula. Integer in volutpat felis. Phasellus ac venenatis mauris. Suspendisse consequat neque vel metus viverra porttitor. Mauris id felis ut tellus sodales hendrerit vel tincidunt lectus. Sed id eros imperdiet, suscipit metus et, consequat enim. Aenean in velit vitae ipsum facilisis blandit. Cras aliquam lectus sit amet augue porta, nec vulputate leo malesuada. Integer tincidunt magna sed tristique tempus. Ut eget urna rutrum, auctor nulla non,luctus metus.Proin sapien ipsum, maximus id vehicula nec, tincidunt ac massa. Donec in fermentum ante, semper maximus sapien. In a erat id turpis hendrerit cursus ut quis tellus. Integer faucibus, ipsum eget rhoncus feugiat, elit ligula cursus risus, sollicitudin rutrum odio orci at urna. Curabitur eget blandit justo, vel finibus sapien. Proin malesuada pellentesque quam, nec ultricies ligula dapibus id. Fusce urna arcu, bibendum in ante at, mattis posuere felis. Vestibulum in laoreet neque. Duis ultricies vehicula felis. Nunc et urna in risus convallis posuere sed sit amet felis. Cras tincidunt est non massa vulputate, ut convallis urna fringilla. Vivamus nec sapien eu mauris finibus bibendum eget sed est. Aliquam posuere, lectus quis dictum sollicitudin, turpis risus rutrum nibh, nec porttitor felis massa a augue.';
+}
